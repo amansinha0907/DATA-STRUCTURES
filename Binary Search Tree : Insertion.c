dfe53b49831975390d08e23f83ed08290a7065b9
Aman Sinha <96279990+amansinha0907@users.noreply.github.com>
2022-09-06 23:49:53 +0530

    Create Binary Search Tree : Insertion.c

---
+++ Binary Search Tree : Insertion.c
@@ -0,0 +1,98 @@
+Skip to content
+Search or jump to…
+Pulls
+Issues
+Marketplace
+Explore
+ 
+@amansinha0907 
+Amit-Kr-Dubey
+/
+Placement_Preparation_2023
+Public
+Code
+Issues
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+
+void preOrder( struct node *root) {
+  
+    if( root == NULL ) 
+      return;
+    printf("%d ",root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+  
+}
+
+/* you only have to complete the function given below.  
+node is defined as  
+struct node {
+    
+    int data;
+    struct node *left;
+    struct node *right;
+  
+};
+*/
+
+struct node* insert( struct node* root, int data ) {
+    
+    if(root == NULL){
+         root = malloc(sizeof(struct node));
+            root->data = data;
+            root->left = NULL;
+            root->right = NULL;
+     
+    }else if(data < root->data){
+            root->left = insert(root->left, data);
+    }else{
+            root->right = insert(root->right, data);
+     }
+    return root;   
+       
+}    
+    
+
+
+int main() {
+  
+    struct node* root = NULL;
+    
+    int t;
+    int data;
+
+    scanf("%d", &t);
+
+    while(t-- > 0) {
+        scanf("%d", &data);
+        root = insert(root, data);
+    }
+  
+    preOrder(root);
+    return 0;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
